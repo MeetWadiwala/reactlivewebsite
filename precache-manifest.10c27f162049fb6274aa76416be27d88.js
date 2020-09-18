476b0262262cb69c9afad16d4f1a781de4e0563b
MeetWadiwala <71484359+MeetWadiwala@users.noreply.github.com>
2020-09-18 17:41:26 +0530

    Updates

---
+++ precache-manifest.10c27f162049fb6274aa76416be27d88.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66533195b281bbc69dcbb923e6383047",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "cebe0670edca2b797ec1",
+    "url": "/reactlivewebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "fa899408858cbdf8e74f",
+    "url": "/reactlivewebsite/static/css/main.4f6f2d3a.chunk.css"
+  },
+  {
+    "revision": "cebe0670edca2b797ec1",
+    "url": "/reactlivewebsite/static/js/2.aa23cfc1.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlivewebsite/static/js/2.aa23cfc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa899408858cbdf8e74f",
+    "url": "/reactlivewebsite/static/js/main.5d5280ff.chunk.js"
+  },
+  {
+    "revision": "48f7b2efd50006fcc042",
+    "url": "/reactlivewebsite/static/js/runtime-main.355cd751.js"
+  },
+  {
+    "revision": "2acd2a27e3903b56ac918a500bf74ed3",
+    "url": "/reactlivewebsite/static/media/hero-img.2acd2a27.png"
+  },
+  {
+    "revision": "091295c192f676a92300c0e57d9c97c1",
+    "url": "/reactlivewebsite/static/media/img2.091295c1.png"
+  },
+  {
+    "revision": "ad368e4549a52d7c656b7d1da930f114",
+    "url": "/reactlivewebsite/static/media/s1.ad368e45.png"
+  },
+  {
+    "revision": "532e3204dd41ed8f801b78ad5244643f",
+    "url": "/reactlivewebsite/static/media/s2.532e3204.png"
+  },
+  {
+    "revision": "354d34eefed2f262157aa63d9b2788fa",
+    "url": "/reactlivewebsite/static/media/s3.354d34ee.png"
+  },
+  {
+    "revision": "861fefec6d3a840d10bcc395a9445fcb",
+    "url": "/reactlivewebsite/static/media/s4.861fefec.png"
+  },
+  {
+    "revision": "455d47e547a2b959e1a672914bf579a3",
+    "url": "/reactlivewebsite/static/media/s5.455d47e5.png"
+  },
+  {
+    "revision": "17f601fd9d07c1e6403f80157ef0ac34",
+    "url": "/reactlivewebsite/static/media/s6.17f601fd.png"
+  }
+]);
