8f0df5d381ebef2b1a48afff4dae09890cc001e2
MeetWadiwala <71484359+MeetWadiwala@users.noreply.github.com>
2020-09-18 12:39:43 +0530

    My first react commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return <>
+        <footer className='w-100 bg-light text-center'>
+            <p>© 2020 MeetTechnical. All Rights Reserved | Terms and conditions applied</p>
+        </footer>
+    </>;
+};
+
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from "./Images/s1.png";
+import web1 from "./Images/s2.png";
+import web2 from "./Images/s3.png";
+import web3 from "./Images/s4.png";
+import web4 from "./Images/s5.png";
+import web5 from "./Images/s6.png";
+
+const Sdata = [
+	{
+		imgsrc: web,
+		title: "web development",
+	},
+	{
+		imgsrc: web1,
+		title: "app development",
+	},
+	{
+		imgsrc: web2,
+		title: "Software development",
+	},
+	{
+		imgsrc: web3,
+		title: "Digital Marketing",
+	},
+	{
+		imgsrc: web4,
+		title: "Android development",
+	},
+	{
+		imgsrc: web5,
+		title: "Marketing",
+	},
+];
+
+export default Sdata;
